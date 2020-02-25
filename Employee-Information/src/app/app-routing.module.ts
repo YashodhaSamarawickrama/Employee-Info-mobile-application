7e180c38aaa0c1603396051f612e49cee3beee95
Yashodha Samarawickrama <yasho.samarawickrama@gmail.com>
2020-02-25 22:11:59 +0530

    login ui update

--- Employee-Information/src/app/app-routing.module.ts
+++ Employee-Information/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'splash',
     loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
   },
   {
